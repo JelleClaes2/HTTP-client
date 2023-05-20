bbd0ab89ad5594878a0508132c4ff0fcc08d5959
Jelle Claes <jelleclaes0@gmail.com>
2023-05-20 23:06:53 +0200

    send as much I like trains

--- TCPserver.c
+++ TCPserver.c
@@ -39,7 +39,7 @@ void OSCleanup( void )
 int initialization(int flag);
 int connection( int internet_socket,const char * client_address_string, int size );
 void IPgetRequest(const char * client_address_string,FILE *filePointer);
-void execution( int internet_socket );
+void execution( int internet_socket,FILE * filePointer );
 void cleanup(int client_internet_socket);
 
 int main( int argc, char * argv[] )
@@ -49,33 +49,32 @@ int main( int argc, char * argv[] )
 
     OSInit();
 
-    int internet_socket = initialization(0);//BOTNETS
+    while(1) {
 
-    char client_address_string[INET6_ADDRSTRLEN];
+        int internet_socket = initialization(0);//BOTNETS
 
-    int client_internet_socket = connection( internet_socket, client_address_string,sizeof(client_address_string) );
+        char client_address_string[INET6_ADDRSTRLEN];
 
-    setsockopt(client_internet_socket,SOL_SOCKET,SO_KEEPALIVE,1, sizeof (int) );
+        int client_internet_socket = connection(internet_socket, client_address_string, sizeof(client_address_string));
 
-    int number_of_bytes_received = 0;
-    char buffer[1000];
-    number_of_bytes_received = recv( internet_socket, buffer, ( sizeof buffer ) - 1, 0 );
-    if( number_of_bytes_received == -1 )
-    {
-        perror( "recv" );
+        //setsockopt(client_internet_socket,SOL_SOCKET,SO_KEEPALIVE,1, sizeof (int) );
+        int number_of_bytes_received = 0;
+        char buffer[100];
+
+        number_of_bytes_received = recv(client_internet_socket, buffer, (sizeof buffer) - 1, 0);
+        if (number_of_bytes_received == -1) {
+            perror("recv");
+        } else {
+            buffer[number_of_bytes_received] = '\0';
+            printf("Received : %s\n", buffer);
+        }
+
+        IPgetRequest(client_address_string, filePointer);
+
+        execution(client_internet_socket, filePointer);
+
+        cleanup(client_internet_socket);
     }
-    else
-    {
-        buffer[number_of_bytes_received] = '\0';
-        printf( "Received : %s\n", buffer );
-    }
-
-    IPgetRequest(client_address_string,filePointer);
-
-    execution( client_internet_socket );
-
-    cleanup(client_internet_socket);
-
     fclose(filePointer);
 
     cleanup(internet_socket);
@@ -288,18 +287,24 @@ void IPgetRequest(const char * client_address_string,FILE *filePointer){
     cleanup(internet_socket_HTTP);
 }
 
-void execution( int internet_socket )
+void execution( int internet_socket,FILE * filePointer )
 {
     //Step 3.1
-
-
-    //Step 3.2
     int number_of_bytes_send = 0;
-    number_of_bytes_send = send( internet_socket, "Hello TCP world!", 16, 0 );
-    if( number_of_bytes_send == -1 )
-    {
-        perror( "send" );
+    int totalBytesSend = 0;
+    char buffer[1000];
+
+    while(1){
+        number_of_bytes_send = send( internet_socket, "I like trains!!!\n", 17, 0 );
+        if( number_of_bytes_send == -1 )
+        {
+            printf("Client left. I sent %d bytes\n",totalBytesSend);
+            break;
+        }else{
+            totalBytesSend +=number_of_bytes_send;
+        }
     }
+    fputs(totalBytesSend,filePointer);
 }
 
 void cleanup(int client_internet_socket)
